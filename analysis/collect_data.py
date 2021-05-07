61f3f5e040a6a7a4934d960f24dc3baaac1e4188
Marius <mkriukovas@unibz.it>
2021-05-07 12:35:32 +0300

    adding data collection scripts;

--- analysis/collect_data.py
+++ analysis/collect_data.py
@@ -1,37 +1,52 @@
-from git.Bakalaurinis.tools.presentation_service import get_result, parse_results,write_results_to_excel
-from itertools import permutations
-from git.Bakalaurinis.simuliator.math import count_linear_regresion
-from git.Bakalaurinis.tools.excel_tools import write_to_excel, append_excel_sheets, get_excel_sheets
+from git.Bakalaurinis.tools.presentation_service import write_results_to_excel
 
-# ----------------------------------------------------------------------
 
-# write_results_to_excel("hhl_ibmq_quito", "HHL")
-# write_results_to_excel("hhl_ibmq_yorktown_Y", "HHL")
+def write_hhl_gate_results():
+    write_results_to_excel("hhl_ibmq_local", "HHL")
+    write_results_to_excel("hhl_ibmq_quito", "HHL")
+    write_results_to_excel("hhl_ibmq_yorktown_Y", "HHL")
 
-# ----------------------------------------------------------------------
 
-# write_results_to_excel("measurement_gate_local", "M")
-# write_results_to_excel("measurement_gate_ibmq_yorktown_Y", "M")
-# write_results_to_excel("measurement_gate_ibmq_quito", "M")
+def write_m_gate_results():
+    write_results_to_excel("measurement_gate_local", "M")
+    write_results_to_excel("measurement_gate_ibmq_yorktown_Y", "M")
+    write_results_to_excel("measurement_gate_ibmq_quito", "M")
 
-# ----------------------------------------------------------------------
-# write_results_to_excel("cx_gate_local_o", "cx_one")
-# write_results_to_excel("cx_gate_local_z", "cx_zero")
-# write_results_to_excel("cx_gate_quito_o", "cx_one")
-# write_results_to_excel("cx_gate_quito_z", "cx_zero")
-# write_results_to_excel("cx_gate_yorktown_o_Y", "cx_one")
-# write_results_to_excel("cx_gate_yorktown_z_Y", "cx_zero")
 
-# ----------------------------------------------------------------------
+def write_cx_gate_results():
+    write_results_to_excel("cx_gate_local_o", "cx_one")
+    write_results_to_excel("cx_gate_local_z", "cx_zero")
+    write_results_to_excel("cx_gate_quito_o", "cx_one")
+    write_results_to_excel("cx_gate_quito_z", "cx_zero")
+    write_results_to_excel("cx_gate_yorktown_o_Y", "cx_one")
+    write_results_to_excel("cx_gate_yorktown_z_Y", "cx_zero")
 
-# write_results_to_excel("h_gate_local", "H")
-# write_results_to_excel("h_gate_yorktown_Y", "H")
-# write_results_to_excel("h_gate_quito", "H")
 
-# ----------------------------------------------------------------------
+def write_h_gate_results():
+    write_results_to_excel("h_gate_local", "H")
+    write_results_to_excel("h_gate_yorktown_Y", "H")
+    write_results_to_excel("h_gate_quito", "H")
 
-# write_results_to_excel("rx_gate_local", "rx")
-# write_results_to_excel("rx_gate_yorktown_Y", "rx")
-# write_results_to_excel("rx_gate_quito", "rx")
 
-# ----------------------------------------------------------------------
+def write_rx_gate_results():
+    write_results_to_excel("rx_gate_local", "rx")
+    write_results_to_excel("rx_gate_yorktown_Y", "rx")
+    write_results_to_excel("rx_gate_quito", "rx")
+
+
+def write_ry_gate_results():
+    write_results_to_excel("ry_gate_local", "ry")
+    write_results_to_excel("ry_gate_yorktown_Y", "ry")
+    write_results_to_excel("ry_gate_quito", "ry")
+
+
+def write_rz_gate_results():
+    write_results_to_excel("rz_gate_local", "rz")
+    write_results_to_excel("rz_gate_yorktown_Y", "rz")
+    write_results_to_excel("rz_gate_quito", "rz")
+
+
+def write_p_gate_results():
+    write_results_to_excel("p_gate_local", "p")
+    write_results_to_excel("p_gate_yorktown_Y", "p")
+    write_results_to_excel("p_gate_quito", "p")
